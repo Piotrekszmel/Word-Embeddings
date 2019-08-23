64735a8e66bd3694de372d94b3f547fecf6202fa
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-08-23 11:36:50 +0200

    created bow model with sklearn CountVectorizer

--- BOW-Ngram-TFIDF/BOW-Ngram-TFIDF.py
+++ BOW-Ngram-TFIDF/BOW-Ngram-TFIDF.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from text_preprocessing import normalize_document
+from sklearn.feature_extraction.text import CountVectorizer
 
 corpus = ['The sky is blue and beautiful.',
           'Love this blue and beautiful sky!',
@@ -19,8 +20,16 @@ corpus_df = pd.DataFrame({'Document': corpus,
                           'Category': labels})
 corpus_df = corpus_df[['Document', 'Category']]
 
-print(normalize_document(corpus[0]))
-print('\n')
 normalize_corpus = np.vectorize(normalize_document)
 norm_corpus = normalize_corpus(corpus)
-print(norm_corpus)
+
+
+#Bag of Words
+cv = CountVectorizer(min_df=0., max_df=1.)
+cv_matrix = cv.fit_transform(norm_corpus)
+cv_matrix = cv_matrix.toarray()
+
+
+c = CountVectorizer(min_df=1, max_df=2)
+c_matrix = c.fit_transform(['I am am am am lol lol', 'am am am I lol','am am' ])
+print(c_matrix.toarray())
