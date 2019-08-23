7cc7416f10b50533000af3c20b6a8e3adf7b265f
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-08-23 11:19:16 +0200

    added function for removing stop words

--- BOW-Ngram-TFIDF/text_preprocessing.py
+++ BOW-Ngram-TFIDF/text_preprocessing.py
@@ -53,5 +53,12 @@ def removing_special_characters(text):
     return text
 
 
-print(removing_special_characters('aa  aa__'))
-print(expand_contractions("I'm you're"))
+def remove_stopwords(text, is_lower_case=False):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    if is_lower_case:
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+    else:
+        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
