bc5aac3d1d6e0728c134d7aae86715f1eeb800ed
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-08-23 11:04:18 +0200

    added strip_html_Tags function to more advanced text preprocessing

--- BOW-Ngram-TFIDF/text_preprocessing.py
+++ BOW-Ngram-TFIDF/text_preprocessing.py
@@ -1,11 +1,19 @@
 import nltk
 import re
+import spacy
+import pandas as pd 
+import numpy as np
+from nltk.tokenize.toktok import ToktokTokenizer
+from bs4 import BeautifulSoup
+from contractions import CONTRACTION_MAP
+import unicodedata
 
 #simple text pre-processing
 wpt = nltk.WordPunctTokenizer()
 stop_words = nltk.corpus.stopwords.words('english')
 
 
+#basic
 def normalize_document(doc):
     doc = re.sub(r'[^a-zA-Z\s]', '', doc, re.I|re.A)
     doc = doc.lower()
@@ -14,3 +22,11 @@ def normalize_document(doc):
     filtered_tokens = [token for token in tokens if token not in stop_words]
     doc = ' '.join(filtered_tokens)
     return doc
+
+
+# more advanced
+nlp = spacy.load('en', parse=False, tag=False, entity=False)
+tokenizer = ToktokTokenizer()
+stopword_list = nltk.corpus.stopword.words('english')
+stopword_list.remove('no')
+stopword_list.remove('not')
